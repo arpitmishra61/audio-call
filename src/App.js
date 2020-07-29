a649c5f452e6099354404b9b36430639c8acc146
Arpit Mishra <37569669+arpitmishra61@users.noreply.github.com>
2020-07-29 15:42:55 +0530

    video added

--- src/App.js
+++ src/App.js
@@ -21,7 +21,8 @@ export default function App() {
   const partnerAudio = useRef()
 
   useEffect(() => {
-    navigator.mediaDevices.getUserMedia({ audio: true })
+
+    navigator.mediaDevices.getUserMedia({ audio: true, video: true })
       .then(audio => {
         //myAudio.current.srcObject = audio
         setMyMedia(audio)
@@ -29,79 +30,81 @@ export default function App() {
       .catch(err => console.log(err))
 
 
+
   }, [])
 
 
   return (
     <div className="app" style={{ display: "flex", justifyContent: "center", flexDirection: "column", alignItems: "center" }}>
-      <h1>Your voice...</h1>
-      <audio controls autoPlay ref={myAudio} />
-
-      <h1>Partner voice...</h1>
 
 
-      <audio controls autoPlay ref={partnerAudio} />
+      <div style={{ width: "90%" }}>  <video controls autoPlay ref={partnerAudio} width="80%" height="500px" /></div>
 
 
-      <h2>Your Id</h2>
-      <h4>{giveRandom()}</h4>
 
-      <h2>Partner Id</h2>
-      <h4>{giveRandom()}</h4>
+      <div>
 
-      <textarea name="cread" id="" cols="30" rows="10" placeholder="Your creadentials" ref={mySecretRef} value={mySecret} onChange={() => {
-        setMySecret(mySecret.current.value)
-      }}></textarea>
 
-      <textarea name="cread" id="" cols="30" rows="10" placeholder="Partner Creadentials" ref={partnerSecretRef} value={partnerSecret} onChange={() => { setPartnerSecret(partnerSecretRef.current.value) }}></textarea >
-      <button onClick={() => {
-        const peer = new Peer({
-          initiator: true,
-          trickle: false,
-          stream: myMedia
 
-        })
+        <textarea name="cread" id="" cols="30" rows="10" placeholder="Your creadentials" ref={mySecretRef} value={mySecret} onChange={() => {
+          setMySecret(mySecret.current.value)
+        }}></textarea>
 
-        peer.on("signal", data => {
-          console.log(data)
-          setMySecret(JSON.stringify(data))
-        })
-        peer.on("stream", stream => {
-          setPartnerMedia(stream)
-          console.log("streaming--1")
-          partnerAudio.current.srcObject = stream
-        })
+        <textarea name="cread" id="" cols="30" rows="10" placeholder="Partner Creadentials" ref={partnerSecretRef} value={partnerSecret} onChange={() => { setPartnerSecret(partnerSecretRef.current.value) }}></textarea >
+        <button onClick={() => {
 
-        setMyPeer(peer)
 
-      }}>Call</button>
-      <button onClick={() => {
-        console.log(myPeer)
-        myPeer.signal(partnerSecret)
-      }}>Accept When partner accept call</button>
-      <button style={{ marginBottom: "30px" }} onClick={() => {
-        console.log("90")
-        const peer1 = new Peer({
-          stream: myMedia,
-          initiator: false,
-          trickle: false
-        })
 
-        peer1.signal(partnerSecret)
-        peer1.on("signal", data => {
-          setMySecret(JSON.stringify(data))
-          console.log(data)
-        })
-        console.log("yee")
-        peer1.on("stream", stream => {
-          console.log("streaming")
-          console.log(stream)
-          setPartnerMedia(stream)
-          partnerAudio.current.srcObject = stream
-        })
 
-      }}>Accept</button>
-    </div>
+          const peer = new Peer({
+            initiator: true,
+            trickle: false,
+            stream: myMedia
+
+          })
+
+          peer.on("signal", data => {
+            console.log(data)
+            setMySecret(JSON.stringify(data))
+          })
+          peer.on("stream", stream => {
+            setPartnerMedia(stream)
+            console.log("streaming--1")
+            partnerAudio.current.srcObject = stream
+          })
+
+          setMyPeer(peer)
+
+        }}>Call</button>
+        <button onClick={() => {
+          console.log(myPeer)
+          myPeer.signal(partnerSecret)
+        }}>Accept When partner accept call</button>
+        <button style={{ marginBottom: "30px" }} onClick={() => {
+          console.log("90")
+
+
+          const peer1 = new Peer({
+            stream: myMedia,
+            initiator: false,
+            trickle: false
+          })
+
+          peer1.signal(partnerSecret)
+          peer1.on("signal", data => {
+            setMySecret(JSON.stringify(data))
+            console.log(data)
+          })
+          console.log("yee")
+          peer1.on("stream", stream => {
+            console.log("streaming")
+            console.log(stream)
+            setPartnerMedia(stream)
+            partnerAudio.current.srcObject = stream
+          })
+
+        }}>Accept</button>
+      </div></div>
   )
 }
 
